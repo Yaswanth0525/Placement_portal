025c74e40c95bfa96c47e84eaec0aaf631a20bb8
Yaswanth0525 <yaswanthbogila@gmail.com>
2025-08-13 23:55:41 +0530

    Updated multer.js and removed vercel.json

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,4 +1,4 @@
 import multer from "multer";
-
 const storage = multer.memoryStorage();
-export const singleUpload = multer({storage}).single("file");
+export const singleUpload = multer({storage}).single("file"); 
+// this middleware is used to handle single file uploads, typically for user profile images or documents
