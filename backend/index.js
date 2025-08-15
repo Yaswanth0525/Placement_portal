9bd1a16b5a863ad442b1efd1cb82c3e1ef4a256e
Yaswanth0525 <yaswanthbogila@gmail.com>
2025-08-16 00:11:11 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: true, // dynamically reflects the request origin
+    origin: allowedOrigins, // dynamically reflects the request origin
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     // allowedHeaders: ["Content-Type", "Authorization"],
